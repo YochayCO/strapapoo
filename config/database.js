72533990c45aa600099017662de5401746e830b9
yochayco <yochai.cohenor@gmail.com>
2021-02-19 13:11:41 +0200

    Yarn cache and correct db env vars

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: "${process.env.DATABASE_HOST}",
-        port: "${process.env.DATABASE_PORT}",
-        database: "${process.env.DATABASE_NAME}",
-        username: "${process.env.DATABASE_USERNAME}",
-        password: "${process.env.DATABASE_PASSWORD}",
+        host: `${process.env.DATABASE_HOST || "localhost"}`,
+        port: `${process.env.DATABASE_PORT || 5432}`,
+        database: `${process.env.DATABASE_NAME || "Rentgen"}`,
+        username: `${process.env.DATABASE_USERNAME || "postgres"}`,
+        password: `${process.env.DATABASE_PASSWORD || "sergy"}`,
       },
       options: {
         ssl: env.bool('DATABASE_SSL', false),
